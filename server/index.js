50c07e142dfb9b5baf1a4d3f37c48a993e87cdbc
Rohit Asture <rohitasture0312@gmail.com>
2022-01-14 11:50:11 +0530

    changed index

--- server/index.js
+++ server/index.js
@@ -10,9 +10,8 @@ app.use('/posts',postRoutes);
 app.use(bodyParser.json({limit:"30mb", extended : true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended : true}));
 app.use(cors());
-const CONNECTION_URL = 'mongodb+srv://rohitAsture:XWDLv8TqdQswC@A@cluster0.lsrdn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.DATABASE,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(() => app.listen(PORT,()=> console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
